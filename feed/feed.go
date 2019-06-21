0ac3701c091bbe7f4b9cf582815a615a7c193369
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-06-21 16:31:10 +0200

    Added omitempty to indicate fields which aren't essential.

--- feed/feed.go
+++ feed/feed.go
@@ -30,24 +30,24 @@ type Channel struct {
 	XMLName     xml.Name   `xml:"channel"`
 	Title       string     `xml:"title"`
 	Website     string     `xml:"link"`
-	Language    string     `xml:"language"`
-	Copyright   string     `xml:"copyright"`
-	Subtitle    string     `xml:"subtitle"`
-	Author      string     `xml:"author"`
+	Language    string     `xml:"language,omitempty"`
+	Copyright   string     `xml:"copyright,omitempty"`
+	Subtitle    string     `xml:"subtitle,omitempty"`
+	Author      string     `xml:"author,omitempty"`
 	Summary     string     `xml:"summary"`
 	Description string     `xml:"description"`
 	Explicit    string     `xml:"explicit"`
 	Image       string     `xml:"image"`
 	Categories  []string   `xml:"category"`
 	Items       []*Episode `xml:"item"`
-	Date        string     `xml:"pubDate"`
+	Date        string     `xml:"pubDate,omitempty"`
 }
 
 type Episode struct {
 	XMLName     xml.Name  `xml:"item"`
 	Title       string    `xml:"title"`
-	Author      string    `xml:"author"`
-	Subtitle    string    `xml:"subtitle"`
+	Author      string    `xml:"author,omitempty"`
+	Subtitle    string    `xml:"subtitle,omitempty"`
 	Summary     string    `xml:"summary"`
 	Description string    `xml:"description"`
 	URL         Enclosure `xml:"enclosure"`
