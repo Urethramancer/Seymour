8d8554a4e3fb619f6fb0eb6190e3944e4f2ec668
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-06-25 22:26:35 +0200

    Use default frequency.

--- config.go
+++ config.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"path/filepath"
+	"time"
 
 	"github.com/Urethramancer/cross"
 )
 
 // Config for Seymour.
 type Config struct {
+	// DownloadPath for the podcast episodes.
 	DownloadPath string `json:"downloadpath"`
+	// Frequency default.
+	Frequency time.Duration `json:"frequency"`
 }
 
 func loadConfig() (*Config, error) {
 	fn := filepath.Join(cross.ConfigPath(), "config.json")
 	var cfg Config
+	cfg.Frequency = time.Hour * 6
 	err := LoadJSON(fn, &cfg)
 	if err != nil {
 		return nil, err
